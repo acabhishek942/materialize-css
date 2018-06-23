2fc6141c167d108cf60544c47dd49650db048fa4
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-23 15:36:25 +0530

    playing around with tables and some js

---
+++ filter.js
@@ -0,0 +1,30 @@
+function filterTable() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("filter_value");
+  filter = parseInt(input.value);
+  table = document.getElementById("threshold_table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    td1 = tr[i].getElementsByTagName("td")[2];
+    if (td && td1) {
+      // modify this condition according to your requirement
+      if (parseInt(td.innerHTML) < filter || parseInt(td1.innerHTML) < filter) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function resetTable() {
+  table = document.getElementById("threshold_table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    tr[i].style.display = "";
+  }
+}
